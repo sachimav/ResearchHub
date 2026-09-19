34435e199595fb18da8236971fbd7b62479ab95c
mavrox7 <thiserasachintha@gmail.com>
2026-09-19 23:22:24 +0530

    create basic server and fix errors

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const startServer = async () => {
+  await connectDB();
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
